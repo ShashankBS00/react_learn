36a7218bf3de973c6e6c4526901003cd7be53d8f
Shashank B S <shashank67676767@gmail.com>
2026-01-16 19:48:50 +0530

    final commit

---
+++ tailwindcss/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div class ='h-screen w-full bg-gray-400 flex items-center justify-center'>
+      
+    </div>
+  )
+}
+
+export default App
